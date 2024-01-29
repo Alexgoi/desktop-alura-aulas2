75dfe99821951eeaa081a7d8d97ddfc3a6c9df01
Alexsandro Goi <86365984+Alexgoi@users.noreply.github.com>
2024-01-29 11:56:44 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
-console.log(numeroSecreto);
 let tentativa = 1;
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    reponsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
 
 function exibirMensagemInicial(){
@@ -33,7 +35,18 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeNumerosNaLista = listaDeNumerosSorteados.length;
+    if(quantidadeDeNumerosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo() {
